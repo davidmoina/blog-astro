bd0f6246f8dfd5941e63c11f7958cd74e90ae753
David Moina <luisdavidmoina@gmail.com>
2025-01-05 14:36:04 +0100

    ✨  Add AuthorCard component, author schemas and relations between collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 // 2. Import loader(s)
 import { glob, file } from "astro/loaders";
@@ -13,10 +13,27 @@ const blog = defineCollection({
       description: z.string(),
       date: z.date(),
       image: image(),
-      author: z.string(),
+      // author: z.string(),
+      // relaciones
+      author: reference("author"),
       tags: z.array(z.string()),
+      isDraft: z.boolean().default(false),
+    }),
+});
+
+const author = defineCollection({
+  loader: glob({ pattern: "**/*.yml", base: "./src/data/author" }),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+      twitter: z.string(),
+      linkedIn: z.string(),
+      github: z.string(),
+      bio: z.string(),
+      subtitle: z.string(),
     }),
 });
 
 // 4. Export a single `collections` object to register your collection(s)
-export const collections = { blog };
+export const collections = { blog, author };
